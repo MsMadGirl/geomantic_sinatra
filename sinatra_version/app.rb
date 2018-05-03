9c4edbde6bc5dcda2ab6943a0482acba7546f44e
Rebecca Lynde-Scott <MsMadGirl@users.noreply.github.com>
2018-05-02 20:42:33 -0700

    refine and comment code

--- sinatra_version/app.rb
+++ sinatra_version/app.rb
@@ -35,6 +35,7 @@ def generate_line
  	# puts "line = #{line_value}"
 end
 
+# generates a mother figure by running generate_line four times, looks up the figure that matches that configuration in the geomantic.figures table, then writes the figure to the geomantic.d_figures table
 def generate_mother (chart_id, fig_position)
 
 	fire_line  = generate_line
@@ -56,6 +57,7 @@ def generate_mother (chart_id, fig_position)
 
 end
 
+#derives daughters from mothers by looking up the line values of all figures in geomantic.c_figures with the correct chart_id, which at this point will only be mothers, assembles the lines of the mothers into the daughters, and writes the new figures to geomantic.c_figures
 def derive_daughter (this_chart_id, d_fig_position)
 
 	if d_fig_position == 1
@@ -70,13 +72,13 @@ def derive_daughter (this_chart_id, d_fig_position)
 		puts "Error: too many elements"
 	end
 
-	puts element
+	# puts element
 
 	result_set = DB.fetch("SELECT * FROM figures INNER JOIN c_figures ON figures.id = c_figures.figure_id WHERE c_figures.chart_id = ? AND c_figures.fig_group = 'Mother' ORDER BY c_figures.id;", this_chart_id).all
-	puts result_set.inspect
+	# puts result_set.inspect
 
 	row = result_set.map{|x| x[:"#{element}"]}
-	puts row
+	# puts row
 
 	fire_line = row[0]
 	air_line = row[1]
@@ -84,7 +86,7 @@ def derive_daughter (this_chart_id, d_fig_position)
 	earth_line = row[3]
 
 	figure_id = DB.fetch('SELECT id FROM figures WHERE fire = ? AND air = ? AND water = ? AND earth = ?', fire_line, air_line, water_line, earth_line).single_value
-	puts "figure id: #{figure_id}"
+	# puts "figure id: #{figure_id}"
 
 	c_figures = DB[:c_figures]
 
@@ -92,6 +94,7 @@ def derive_daughter (this_chart_id, d_fig_position)
 
 end
 
+#adds lines to get odds or evens for new line
 def add_line(line1, line2)
 
 	if (line1 + line2).even?
@@ -102,6 +105,7 @@ def add_line(line1, line2)
 
 end
 
+#adds figures by running add_line for each line
 def add_figure(figure1, figure2)
 
 	figure = Hash.new
@@ -114,30 +118,24 @@ def add_figure(figure1, figure2)
 	return figure
 end
 
+#derives nieces by looking up the line values of all figures with the correct chart_id, all of which will be mothers or daughters, iterates through each pair of previous figures, adds them together using add_figure, looks up the new figure in geomantic.figures, and adds the new figures to geomantic.c_figures
 def derive_niece (this_chart_id)
 
 	#fetch figures from db
 
 	result_set = DB.fetch('SELECT fire, air, water, earth FROM figures INNER JOIN c_figures on figures.id = c_figures.figure_id WHERE c_figures.chart_id = ? ORDER BY c_figures.id;', this_chart_id).all
 
-	puts "~~~~~~~"
-	puts result_set
-	puts "~~~~~~~"
-
-	#shift to remove figures from the front of the array, in some kind of loop
-	#add lines four times
-	#create figure
-	#do four times to create nieces
-
-#oops, the array indices are wrong, figure out what the correct indices are
+	# puts "~~~~~~~"
+	# puts result_set
+	# puts "~~~~~~~"
 
 	(1..4).each do |e|
 
-		puts "Niece #{e}"
+		# puts "Niece #{e}"
 		figure1 = result_set.shift
-		puts figure1
+		# puts figure1
 		figure2 = result_set.shift
-		puts figure2
+		# puts figure2
 
 		figure = add_figure(figure1, figure2)
 
@@ -146,7 +144,7 @@ def derive_niece (this_chart_id)
 		# puts "*****"
 
 		figure_id = DB.fetch('SELECT id FROM figures WHERE fire = ? AND air = ? AND water = ? AND earth = ?', figure[:fire_line], figure[:air_line], figure[:water_line], figure[:earth_line]).single_value
-		puts figure_id
+		# puts figure_id
 
 		c_figures = DB[:c_figures]
 
@@ -155,23 +153,24 @@ def derive_niece (this_chart_id)
 	
 end
 
+#derives witnesses by looking up the nieces and doing the same thing derive_niece did
 def derive_witness(this_chart_id)
 
 	result_set = DB.fetch("SELECT fire, air, water, earth FROM figures INNER JOIN c_figures on figures.id = c_figures.figure_id WHERE c_figures.chart_id = ? AND c_figures.fig_group = 'Niece' ORDER BY c_figures.id;", this_chart_id).all
 
 	(1..2).each do |e|
 
-		puts "Witness #{e}"
+		# puts "Witness #{e}"
 
 		figure1 = result_set.shift
-		puts figure1
+		# puts figure1
 		figure2 = result_set.shift
-		puts figure2
+		# puts figure2
 
 		figure = add_figure(figure1, figure2)
 
 		figure_id = DB.fetch('SELECT id FROM figures WHERE fire = ? AND air = ? AND water = ? AND earth = ?', figure[:fire_line], figure[:air_line], figure[:water_line], figure[:earth_line]).single_value
-		puts figure_id
+		# puts figure_id
 
 		c_figures = DB[:c_figures]
 
@@ -180,43 +179,46 @@ def derive_witness(this_chart_id)
 
 end
 
+#derives judge by looking up the witnesses and doing the same thing derive_niece did, plus running the checksum of making sure that the Judge has an even number of dots, a checksum built into the geomantic chart
 def derive_judge(this_chart_id)
 
 	result_set = DB.fetch("SELECT fire, air, water, earth FROM figures INNER JOIN c_figures on figures.id = c_figures.figure_id WHERE c_figures.chart_id = ? AND c_figures.fig_group = 'Witness' ORDER BY c_figures.id;", this_chart_id).all
 
-	puts "Judge"
-	puts result_set
+	# puts "Judge"
+	# puts result_set
 
 	figure1 = result_set.shift
-	puts figure1
+	# puts figure1
 	figure2 = result_set.shift
-	puts figure2
+	# puts figure2
 
 	figure = add_figure(figure1, figure2)
 
-	if (figure[:fire_line] + figure[:air_line] + figure[:water_line] + figure[:earth_line]).odd?
-		puts "error"
-	end
-
 	figure_id = DB.fetch('SELECT id FROM figures WHERE fire = ? AND air = ? AND water = ? AND earth = ?', figure[:fire_line], figure[:air_line], figure[:water_line], figure[:earth_line]).single_value
-		puts figure_id
+		# puts figure_id
 
 	c_figures = DB[:c_figures]
 
 	c_figures.insert(:chart_id => this_chart_id, :figure_id => figure_id, :fig_group => 'Judge', :fig_position => 1)
 
-end
+	if (figure[:fire_line] + figure[:air_line] + figure[:water_line] + figure[:earth_line]).odd?
+		error = "<p style=\"color:red;\">Error: Judge is odd. If you see this error, please email hexdotink@gmail.com</p>"
+	else 
+		error = ""
+	end
 
-def get_date
-	chart_date = Date.today
 end
 
 ##########################
+#routes below this
+##########################
 
+#gets the index, which has the chart metadata form
 get '/' do
 	erb :index
 end
 
+#posts the params from the index form, stores them in the geomantic.c_metadata table, creates and displays the shield chart
 post '/chart' do
 	
 	chart_name = params[:chart_name]
@@ -225,7 +227,7 @@ post '/chart' do
 	chart_subject = params[:chart_subject]
 	chart_date = Date.today
 	
-	puts chart_name
+	# puts chart_name
 
 	c_metadata = DB[:c_metadata]
 
@@ -249,7 +251,7 @@ post '/chart' do
 
 	derive_witness(this_chart_id)
 
-	derive_judge(this_chart_id)
+	error = derive_judge(this_chart_id)
 
 	chart_figures = DB.fetch('SELECT cf.chart_id, cf.figure_id, cf.fig_group, cf.fig_position, f.name, f.translation, f.image
 		FROM c_figures as cf
@@ -257,9 +259,7 @@ post '/chart' do
 		ON cf.figure_id = f.id
 		WHERE cf.chart_id = ?
 		ORDER BY cf.id;', this_chart_id).all
-	puts chart_figures
-
-	#note to self: be very careeful of order, subtle errors can creep in here
+	# puts chart_figures
 
 	slot_names = ['mother1', 'mother2', 'mother3', 'mother4', 'daughter1', 'daughter2', 'daughter3', 'daughter4', 'niece1', 'niece2', 'niece3', 'niece4', 'witness1', 'witness2', 'judge']
 
@@ -269,11 +269,8 @@ post '/chart' do
 	   slot_figures["#{s}"] = chart_figures.shift
 	end
 
-	puts slot_figures
+	# puts slot_figures
 
-		# figure = result_set.shift
-		#grades["Dorothy Doe"] = 9
-
-	erb :chart, :locals => {:chart_date => chart_date, :chart_name => chart_name, :chart_by => chart_by, :chart_for => chart_for, :chart_subject => chart_subject, :this_chart_id => this_chart_id, :slot_figures => slot_figures}
+	erb :chart, :locals => {:chart_date => chart_date, :chart_name => chart_name, :chart_by => chart_by, :chart_for => chart_for, :chart_subject => chart_subject, :this_chart_id => this_chart_id, :slot_figures => slot_figures, :error => error}
 
 end
